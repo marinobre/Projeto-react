aa8c2e2e0480373482f24140a34ff47b16e9ad52
mari <marizelianobre@gmail.com>
2021-11-03 11:14:05 -0300

    organizando componentes e pages

--- prova-react/src/App.js
+++ prova-react/src/App.js
@@ -1,9 +1,11 @@
 import './App.css';
+import Footer from './components/template/Footer';
+
 
 function App() {
   return (
     <>
-    teste
+    <Footer/>
     </>
   );
 }
---
+++ prova-react/src/components/template/Footer.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+function Footer () {
+
+return(
+
+ <div className="dashboard">
+ <p>&copy 2020 Collective. All Rights Reserved | Design by <a href="https://w3layouts.com/" target="_blank"
+     className="text-primary">W3layouts.</a></p>
+</div>
+);
+
+}
+
+export default Footer;
