5b824bfbbd5fc1671f6ac11277e8d5d16777428e
mari <marizelianobre@gmail.com>
2021-11-03 12:12:59 -0300

    criando componentes pages

--- prova-react/src/App.js
+++ prova-react/src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import Footer from './components/template/Footer';
-
+import Home from './pages/Home';
 
 function App() {
   return (
     <>
+    {/* <Header/> */}
+    <Home/>
     <Footer/>
     </>
   );
